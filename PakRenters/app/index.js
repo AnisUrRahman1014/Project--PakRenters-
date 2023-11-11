1387bce276321474d2c630d4afcd63fe2ea89ad3
Anis Ur Rahman <78692422+AnisUrRahman1014@users.noreply.github.com>
2023-11-11 18:12:47 +0500

    Setting environment to begin app

--- PakRenters/app/index.js
+++ PakRenters/app/index.js
@@ -1,34 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Touchable, Dimensions } from "react-native";
+
+let mainBlue = '#182978';
+let midBlue = '#6688CC';
+let baseBlue = '#ACBFE6';
 
 export default function Page() {
   return (
     <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+      <View style={styles.navBar}>
+        <Text>Hello World</Text>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
