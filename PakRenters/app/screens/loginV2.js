066cfbd94d68407ba0d5478182b63e7b375fb92b
Anis Ur Rahman <78692422+AnisUrRahman1014@users.noreply.github.com>
2024-02-27 14:20:49 +0500

    Signup Container

--- PakRenters/app/screens/loginV2.js
+++ PakRenters/app/screens/loginV2.js
@@ -41,12 +41,91 @@ const LoginV2 = () => {
             width: wp(50),
             height: hp(25),
             resizeMode: "contain",
-            position: "relative"
+            position: "relative",
+            flex: 1
           }}
         />
         <Text style={styles.tagLineLabel}>Please sign in to continue</Text>
         <View style={styles.componentsContainer}>
-          <Text>Hello World</Text>
+          {/*  Username Input Field */}
+          <View style={styles.inputFieldContainer}>
+            <View style={styles.inputField}>
+              <Icon
+                name="user"
+                color={Color.dark}
+                size={30}
+                paddingRight={5}
+                paddingVertical={4.7}
+              />
+              <TextInput
+                placeholder="Enter Username or Email"
+                color={Color.dark}
+                style={styles.inputFieldStyle}
+                value={username}
+                onChangeText={usernameFieldHandler}
+              />
+            </View>
+            {/* Separator Bar */}
+            <View
+              style={{
+                width: "98%",
+                height: "2%",
+                backgroundColor: Color.dark
+              }}
+            />
+          </View>
+
+          {/* PASSWORD INPUT FIELD */}
+          <View style={styles.inputFieldContainer}>
+            <View style={styles.inputField}>
+              <Icon
+                name="lock"
+                color={Color.dark}
+                size={30}
+                paddingRight={5}
+                paddingVertical={4.7}
+              />
+              <TextInput
+                placeholder="Enter Password"
+                color={Color.dark}
+                style={styles.inputFieldStyle}
+                value={password}
+                onChangeText={passwordFieldHandler}
+                secureTextEntry={true}
+              />
+            </View>
+            {/* Separator Bar */}
+            <View
+              style={{
+                width: "98%",
+                height: "2%",
+                backgroundColor: Color.dark
+              }}
+            />
+          </View>
+
+          {/* FORGET PASSWORD BUTTON */}
+          <View style={styles.forgetPassContainer}>
+            <TouchableOpacity>
+              <Text style={styles.forgetPassLabel}>Forgot Password?</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Login Button */}
+          <View style={styles.loginBtnContainer}>
+            <TouchableOpacity style={styles.loginBtn}>
+              <Text style={styles.loginLabel}>Login</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {/* SIGN UP LABEL */}
+        <View style={styles.signUpContainer}>
+          <TouchableOpacity>
+            <Text style={styles.signUpLabel}>
+              Don't have an account? Sign up
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
     </SafeAreaView>
@@ -68,15 +147,69 @@ const styles = {
     width: "90%",
     height: "70%",
     borderRadius: wp(3),
-    backgroundColor: Color.dark,
-    alignItems: "center",
+    alignItems: "start",
     paddingVertical: "8%",
-    paddingHorizontal: "4%"
+    paddingHorizontal: "4%",
+    flex: 3
   },
   tagLineLabel: {
     fontFamily: FontFamily.ubuntuRegular,
     fontSize: 24,
-    marginBottom: hp(2)
+    flex: 0.3
+  },
+  inputFieldStyle: {
+    position: "relative",
+    fontFamily: FontFamily.ubuntuRegular,
+    fontSize: 18,
+    width: "90%",
+    height: hp(5)
+  },
+  inputField: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  inputFieldContainer: {
+    paddingVertical: 8
+  },
+  forgetPassContainer: {
+    position: "relative",
+    width: "98%",
+    justifyContent: "center",
+    alignItems: "space-between"
+  },
+  forgetPassLabel: {
+    fontFamily: FontFamily.ubuntuLight,
+    color: Color.dark
+  },
+  loginBtnContainer: {
+    position: "relative",
+    width: "98%",
+    height: hp(25),
+    justifyContent: "top",
+    alignItems: "center",
+    paddingVertical: 16
+  },
+  loginBtn: {
+    position: "relative",
+    backgroundColor: Color.dark,
+    width: wp(50),
+    height: "50%",
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  loginLabel: {
+    fontFamily: FontFamily.ubuntuRegular,
+    fontSize: 16,
+    color: Color.white
+  },
+  signUpContainer: {
+    paddingVertical: 8
+  },
+  signUpLabel: {
+    fontFamily: FontFamily.ubuntuRegular,
+    fontSize: 16,
+    color: Color.dark
   }
 };
 
