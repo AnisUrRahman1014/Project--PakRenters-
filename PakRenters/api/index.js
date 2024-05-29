e4b7ee165e5e134baa8205d39e0976376c2f4cf8
Salman Shahid <Maansheikh94@gmail.com>
2024-05-29 22:14:47 +0500

    chat

--- PakRenters/api/index.js
+++ PakRenters/api/index.js
@@ -13,7 +13,7 @@ app.use("/uploads", express.static("uploads"));
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to the database"))
-  .catch(error => console.log("Error connecting to MongoDB", error));
+  .catch((error) => console.log("Error connecting to MongoDB", error));
 
 // Routes
 const authRoutes = require("./routes/auth");
@@ -30,6 +30,52 @@ app.get("/", (req, res) => {
   res.send({ status: "Started" });
 });
 
-app.listen(port, () => {
+var server = app.listen(port, () => {
   console.log("Server is running on port", port);
+});
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: [
+      "http://192.168.0.116:8081",
+      "http://localhost:19006",
+      "http://localhost:19007",
+      "http://localhost:19008",
+      "http://localhost:3001",
+    ],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  socket.on("setup", (id) => {
+    console.log("USER CONNECTED" + id);
+    socket.join(id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+
+  socket.on("new message", (newMessageRecieved) => {
+    console.log(newMessageRecieved);
+    var chat = newMessageRecieved.chatId;
+    socket.in(chat).emit("message recieved", newMessageRecieved);
+    // if (!chat.users) return console.log("chat.users not defined");
+    // chat.users.forEach((user) => {
+    //   if (user._id == newMessageRecieved.sender._id) return;
+    //   socket.in(user._id).emit("message recieved", newMessageRecieved);
+    // });
+  });
+
+  socket.off("setup", (userData) => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
 });
--- PakRenters/app/screens/chatScreen.jsx
+++ PakRenters/app/screens/chatScreen.jsx
@@ -3,19 +3,26 @@ import {
   ScrollView,
   KeyboardAvoidingView,
   TouchableOpacity,
-  Image
+  Image,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet } from "react-native";
 import { Color, sizeManager } from "../../constants/GlobalStyles";
 import { Stack, useLocalSearchParams } from "expo-router";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { CustomFormInputField } from "../../components/misc";
 import Message from "../../components/message";
+import axios from "axios";
+import { io } from "socket.io-client";
 
-const ChatScreen = () => {
-  const { username } = useLocalSearchParams();
+var socket;
+
+const ChatScreen = ({ user, sender }) => {
+  const { username, chatId } = useLocalSearchParams();
   const [messageText, setMessageText] = useState("");
+  const [messageInput, setMessageInput] = useState("");
+  const [isTyping, setIsTyping] = useState(false);
+  // const name = sender.name ? sender.name : "";
   //   Dummy Data
   const [messages, setMessages] = useState([
     { from: "you", message: "Hi", time: "07:21 pm" },
@@ -24,42 +31,119 @@ const ChatScreen = () => {
     {
       from: "you",
       message: "When was the car tuned last time?",
-      time: "07:25 pm"
+      time: "07:25 pm",
     },
     {
       from: "you",
       message: "Hmny Naran tk jana hai, koi issue tu nhi hoga?",
-      time: "07:27 pm"
+      time: "07:27 pm",
     },
     {
       from: "them",
       message: "G me bhi thik. Last week hi tune krwayi hai",
-      time: "07:30 pm"
+      time: "07:30 pm",
     },
     {
       from: "them",
       message:
         "aap befikr ho k jayien, kehty hain tu me tasali bhi krwa donga ap check krlien",
-      time: "07:33 pm"
+      time: "07:33 pm",
     },
-    { from: "you", message: "Ok, milty hain In sha allah", time: "07:40 pm" }
+    { from: "you", message: "Ok, milty hain In sha allah", time: "07:40 pm" },
   ]);
+
+  useEffect(() => {
+    socket = io("http://192.168.0.116:8000");
+    socket.emit("setup");
+    socket.on("connected", () => {
+      console.log("connected");
+      getMessages();
+    });
+  }, []);
+
+  const getMessages = () => {
+    // axios
+    //   .get(`http://localhost:3000/api/messages/${chatId}`, {
+    //     headers: {
+    //       "x-auth-token": localStorage.getItem("token"),
+    //     },
+    //   })
+    //   .then((res) => {
+    //     var tempMessages = [];
+    //     res.data.map((msg) => {
+    //       var message = msg;
+    //       var createdAt = new Date(msg.createdAt);
+    //       var time = createdAt.toLocaleTimeString([], {
+    //         hour: "numeric",
+    //         minute: "2-digit",
+    //         hour12: true,
+    //       });
+    //       message.time = time;
+    //       tempMessages.push(message);
+    //     });
+    //     setMessages(tempMessages);
+    //   })
+    //   .catch((err) => console.log(err));
+    socket.emit("join chat", chatId);
+  };
+
+  useEffect(() => {
+    socket.on("message recieved", (msg) => {
+      setMessages([...messages, msg]);
+    });
+  });
 
   const handleOnSendBtn = () => {
     if (messageText.trim() !== "") {
       const newMessage = {
-        from: "you",
+        chatId: chatId,
         message: messageText,
+        sender: 1,
         time: new Date().toLocaleTimeString([], {
           hour: "2-digit",
           minute: "2-digit",
-          hour12: true
-        })
+          hour12: true,
+        }),
       };
-      setMessages(prevMessages => [...prevMessages, newMessage]);
+      socket.emit("new message", newMessage);
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
       setMessageText("");
     }
   };
+  // const handleSendMessage = () => {
+  //   socket.emit("stop typing", chatId);
+
+  //   if (messageInput.trim() !== "") {
+  //     const message = {
+  //       chatId: chatId,
+  //       message: messageInput,
+  //       sender: user._id,
+  //     };
+  //     axios
+  //       .post("http://localhost:3000/api/messages", message, {
+  //         headers: {
+  //           "x-auth-token": localStorage.getItem("token"),
+  //         },
+  //       })
+  //       .then((res) => {
+  //         var msg = res.data;
+  //         var createdAt = new Date(msg.createdAt);
+  //         var time = createdAt.toLocaleTimeString([], {
+  //           hour: "numeric",
+  //           minute: "2-digit",
+  //           hour12: true,
+  //         });
+  //         msg.time = time;
+  //         socket.emit("new message", msg);
+  //         setMessages([...messages, msg]);
+  //         setMessageInput("");
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   }
+  // };
+
   return (
     <KeyboardAvoidingView style={{ flex: 1 }}>
       <Stack.Screen
@@ -69,7 +153,7 @@ const ChatScreen = () => {
           headerTintColor: Color.white,
           headerStyle: { backgroundColor: Color.dark },
           headerTitleAlign: "center",
-          headerRight: () =>
+          headerRight: () => (
             <TouchableOpacity
               style={{
                 position: "relative",
@@ -80,7 +164,7 @@ const ChatScreen = () => {
                 borderRadius: sizeManager(1),
                 alignItems: "center",
                 justifyContent: "center",
-                elevation: 5
+                elevation: 5,
               }}
             >
               <MaterialIcons
@@ -89,6 +173,7 @@ const ChatScreen = () => {
                 color={Color.dark}
               />
             </TouchableOpacity>
+          ),
         }}
       />
       {/* MESSAGES CONTAINER */}
@@ -96,18 +181,18 @@ const ChatScreen = () => {
         style={{
           height: sizeManager(2),
           marginBottom: sizeManager(0.5),
-          backfaceVisibility: "visible"
+          backfaceVisibility: "visible",
         }}
         contentContainerStyle={{ paddingVertical: sizeManager(1) }}
       >
-        {messages.map((message, index) =>
+        {messages.map((message, index) => (
           <Message
             key={index}
             sender={message.from}
             message={message.message}
             time={message.time}
           />
-        )}
+        ))}
       </ScrollView>
       <View style={styles.inputContainer}>
         <View style={styles.inputContainer.iconPropsContainer}>
@@ -141,7 +226,7 @@ const ChatScreen = () => {
 
 const styles = StyleSheet.create({
   mainContainer: {
-    flex: 1
+    flex: 1,
   },
   inputContainer: {
     flex: 0.07,
@@ -152,21 +237,21 @@ const styles = StyleSheet.create({
     backgroundColor: Color.white,
     gap: sizeManager(1),
     fieldContainer: {
-      flex: 0.9
+      flex: 0.9,
     },
     iconPropsContainer: {
       flex: 0.1,
       flexDirection: "row",
       gap: sizeManager(0.5),
       alignItems: "center",
-      justifyContent: "center"
+      justifyContent: "center",
     },
     sendBtnContainer: {
       flex: 0.1,
       alignItems: "center",
-      justifyContent: "center"
-    }
-  }
+      justifyContent: "center",
+    },
+  },
 });
 
 export default ChatScreen;
